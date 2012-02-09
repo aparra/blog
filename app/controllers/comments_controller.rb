f77db1d54dff639e5e9d35170d907948cbd2a953
Anderson Parra <ander.pp@gmail.com>
2012-02-09 00:25:21 -0200

    add authenticate filter in restriction actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  before_filter :load_article
+  before_filter :load_article, :except => :destroy
+  before_filter :authenticate, :only => :destroy
 
   def create
     @comment = @article.comments.new(params[:comment])
@@ -12,6 +13,7 @@ class CommentsController < ApplicationController
   end
   
   def destroy
+    @article = current_user.articles.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to @article, :notice => 'Comment deleted'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
 
   def new
     @user = User.new
@@ -15,11 +16,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   end
   
   def update
-    @user = User.find(params[:id])
+    @user = current_user
     
     if @user.update_attributes(params[:user])
       redirect_to articles_path, :notice => 'Update user information successfuly.'
